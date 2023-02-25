72def27ad54a55f30d847433dfac439401b39b71
SyuanTsai <carsun00@gmail.com>
2023-02-25 17:07:53 +0800

    Refactor and create first Test.

--- WhoLikesIt.UnitTest/WhoLikesIt.Test.cs
+++ WhoLikesIt.UnitTest/WhoLikesIt.Test.cs
@@ -2,16 +2,19 @@ namespace WhoLikesIt.UnitTest;
 
 public class WhoLikesItTest
 {
+    private WhoLikesIt? _sut;
+
     [SetUp]
     public void Setup()
     {
+        _sut = new WhoLikesIt();
     }
 
     [Test]
     public void Test1()
     {
-        var sut = new WhoLikesIt();
         List<string> data = new List<string>();
-        sut.Likes(data);
+        var act =  _sut.Likes(data);
+        Assert.That( act, Does.EndWith("likes this"), "End string should be `likes this`");
     }
 }
